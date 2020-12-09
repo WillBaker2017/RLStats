473a7d261ba35eab21966b0e3f82c392a82f9c98
WillBaker2017 <will.baker2017@comcast.net>
2020-12-09 15:31:07 -0500

    Added HTML Files, Added Backend To Project to Handle API Parsing

--- RLStats/Pages/Privacy.cshtml.cs
+++ RLStats/Pages/Privacy.cshtml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -17,12 +18,17 @@ namespace RLStats.Pages
             _logger = logger;
         }
 
-        public void OnPost()
+        public async Task OnPostAsync()
         {
-            
+
+            using var client = new HttpClient();
+
+            var content = await client.GetStringAsync("https://api.tracker.gg/api/v2/rocket-league/standard/profile/xbl/canihaveacheeto");
+
+
             var inGameName = Request.Form["ingamename"];
             
-            System.Diagnostics.Debug.WriteLine(inGameName);
+            System.Diagnostics.Debug.WriteLine(content);
             
         }
 
